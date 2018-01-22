7b9a28ad9a34f553d7af4daf2e2a911c452543ad
DenimCity <altidorj1@gmail.com>
2018-01-22 16:24:57 -0500

    created schema file and formatted with what i believe i need

---
+++ db/schema.js
@@ -0,0 +1,44 @@
+//connecting mongoose 
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+mongoose.Promise = global.Promise
+
+
+
+const PhotosSchema = new Schema({
+  img1:{type:String},
+  img2: {type:String},
+  img3: {type: String}
+})
+
+const PhotographerSchema = new Schema({
+  firstName: {type: String},
+  lastName:{type: String},
+  website: {type: String},
+  photo: {type: String}
+},
+  {usePushEach: true}
+)
+
+const UserSchema = new Schema({
+  firstName: {type: String},
+  lastName: {type: String},
+  instagram: {type: String},
+  camera: {type: String},
+  lens: {type: String},
+  photo: {type: String},
+    default: 'http://www.clipartlord.com/wp-content/uploads/2014/04/egg4.png',
+  photographers: [PhotographerSchema],
+  photos:[PhotosSchema]
+},
+
+{usePushEach: true},
+) 
+
+module.exports ={
+  UserSchema,
+  PhotographerSchema,
+  PhotosSchema
+}
