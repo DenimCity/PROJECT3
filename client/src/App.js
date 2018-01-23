ba1b6ebfa0bbd15ef1eff4829470abc0f8fa7a5d
DenimCity <altidorj1@gmail.com>
2018-01-23 10:42:44 -0500

    precreated router and link module

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
+import bg from './pictures/userphotos/navBar background.jpg'
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
+
+  // <Router>
+      
+      {/* <Link to=''></Link>
+      <Link to=''></Link>
+      <Link to=''></Link>
+      <Link to=''></Link>
+      <Link to=''></Link>
+      <Link to=''></Link> */}
+ {/* </Router> */}
+ 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+    
+      
+        <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={bg} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+
     );
   }
 }
