beab8e9f106cad57969830dfd32df303a1d252a5
DenimCity <altidorj1@gmail.com>
2018-01-23 14:03:58 -0500

    created teh styling for hte home page... refactoring the links

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,61 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import bg from './pictures/userphotos/navBar background.jpg'
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import User from './components/User'
 
+// <Router>
 
-  // <Router>
-      
-      {/* <Link to=''></Link>
+{/* <Link to=''></Link>
       <Link to=''></Link>
       <Link to=''></Link>
       <Link to=''></Link>
       <Link to=''></Link>
-      <Link to=''></Link> */}
- {/* </Router> */}
- 
+      <Link to=''></Link> */
+}
+{/* </Router> */
+}
+
 class App extends Component {
   render() {
     return (
-    
-      
-        <div className="App">
-        <header className="App-header">
-          <img src={bg} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="body">
+          <div className="HomePageBackground">
+            <span className="homePageText">
+              <h1>Welcome to ClickedIn!</h1>
+            </span>
+          </div>
+          <nav className="navbar">
 
+            <div className="buttonLeftCorner">
+              <a href="#home">ClickedIn</a>
+            </div>
+            <div className="buttonRightContainer">
+              <div className="buttonRight">
+                <Link to="/User">Users</Link>
+
+              </div>
+              <div className="buttonRight">
+                <a href="http://">Photos</a>
+              </div>
+            </div>
+          </nav>
+          <div className="homePageBtmContainer">
+            <h2>Welcome Visual Creators</h2>
+
+            <div className="textContainer">
+              <p>
+                A page dedicated to photographers and their "Photogmanship. Come one come all.
+                Become a user and upload away.
+              </p>
+            </div>
+          </div>
+        </div>
+
+      </Router>
     );
   }
 }
