123a431dc8f4e309d9b565d8ff9c1a832dbd519e
DenimCity <altidorj1@gmail.com>
2018-01-22 21:17:28 -0500

    dont  need the index controller

--- routes/indexController.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-///to get the homepage
-
-
-router.get('/', (req,res)=>{
-  res.render('index', {title: "Welcome To ClickedIn!"})
-})
