0b76b8a7d95a9c218e2225d8a162f40176abb0fa
DenimCity <altidorj1@gmail.com>
2018-01-23 21:56:14 -0500

    the database is now captured and  ready to display on the page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch,} from 'react-router-dom'
 import './App.css';
 import axios from 'axios'
 import HomePage from './components/HomePage'
@@ -11,15 +11,31 @@ import UserPage from './components/UserPage'
 
 class App extends Component {
 
+state = {
+  users:[]
+}
+
+userDatabase = ()=> {
+  return (axios.get('/api/users')
+.then(response =>{
+  const users = response.data
+  this.setState({users:users})
+}))
+}
+ componentWillMount(){
+   this.userDatabase()
+ }
   render() {
+
+    const userInfo =()=> (<UserPage users={this.state.users}/>)
     return (
       <Router>
         <Switch>
         <Route exact path="/" component={HomePage}/>
-        <Route exact path="/user" component={UserPage}/>
+        <Route exact path="/user" component={userInfo}/>
         </Switch>
       </Router>
-    );
+    )
   }
 }
 
