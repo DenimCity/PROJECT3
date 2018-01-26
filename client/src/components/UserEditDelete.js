50255571b70cded038d63879ebf9f963f586ebe0
DenimCity <altidorj1@gmail.com>
2018-01-26 16:15:56 -0500

    the form now populated the form for editting

--- client/src/components/UserEditDelete.js
+++ client/src/components/UserEditDelete.js
@@ -1,140 +1,121 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import axios from 'axios'
-
 
 class UserEditDelete extends Component {
 
- 
-
-  state={
-    users: [],
-    photo:"",
-    firstName:"",
-    lastName:"",
-    instagram:"",
-    camera:"",
-    lens:"",
-
-    
+  state = {
+    user: {
+      photo: "http://www.clipartlord.com/wp-content/uploads/2014/04/egg4.png",
+      firstName: "",
+      lastName: "",
+      instagram: "",
+      camera: "",
+      lens: ""
+    }
 
   }
-
-
-
-  updateState = () => {
-    axios.get(`/api/users/${this.state.user._id}`, this.state.user)
-    .then(res => {
-      const userId = this.props.match.params.userId
-      const users = this.props.users
-      console.log(userId)
-
-    })
-    // FIND THE USER WITH THE ID IN THIS.PROPS.MATCH.PARAMS.
-    // UPDATE TH STATE WITH THAT USER
-    const userId = this.props.match.params.userId
-    const users = this.props.users
-    console.log(userId)
-    this.setState((users))
+  handleChange = (event) => {
+   
+    const attribute = event.target.name
+    let result = event.target.value
+    const updatedUser = {...this.state.updatedUser}
+    updatedUser[attribute] = result
   }
- 
-  userDatabase = () => {
+
+  updateCurrentState = () => {
     axios
-      .get('/api/users')
+      .get(`/api/users/${this.props.match.params.userId}`)
+     
       .then(response => {
-        const users = response.data
-        this.setState({users: users})
+        this.setState({user: response.data})
+        
+      })
+      .catch((error) => {
+        console.log(error)
       })
   }
 
-
   componentWillMount() {
-    this.updateState()
-
+    this.updateCurrentState()
 
   }
-  
-  
-  render(){
+
+  render() {
     
-
-
     return (
-<div>
-      
       <div>
+
         <div>
-          <h2>Update User</h2>
+          <div>
+            <h2>Update User</h2>
+          </div>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <input
+                onChange={this.handleChange}
+                name="photo"
+                placeholder="DefaultPhoto"
+                type="text"
+                value={this.state.user.photo}/>
+            </div>
+            <div>
+              <input
+                onChange={this.handleChange}
+                name="firstName"
+                placeholder="first name"
+                type="text"
+                value={this.state.user.firstName}/>
+            </div>
+            <div>
+              <input
+                onChange={this.handleChange}
+                name="lastName"
+                placeholder="last name"
+                type="text"
+                value={this.state.user.lastName}/>
+            </div>
+            <div>
+              <input
+                onChange={this.handleChange}
+                name="instagram"
+                placeholder="@instagram"
+                type="text"
+                value={this.state.user.instagram}/>
+            </div>
+            <div>
+              <input
+                onChange={this.handleChange}
+                name="camera"
+                placeholder="Go to Camera"
+                type="text"
+                value={this.state.user.camera}/>
+            </div>
+            <div>
+              <input
+                onChange={this.handleChange}
+                name="lens"
+                placeholder="go to lens"
+                type="text"
+                value={this.state.user.lens}/>
+            </div>
+            <div>
+              <input
+                onChange={this.handleChange}
+                name="photos"
+                placeholder="a picture of your most recent work"
+                type="text"
+                value={this.state.user.img1}/>
+            </div>
+            <button type="readonly">
+              Submit
+            </button>
+          </form>
+       
+          
         </div>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <input
-              onChange={this.handleChange}
-              name="photo"
-              placeholder="DefaultPhoto"
-              type="text"
-              value={this.state.photo}/>
-          </div>
-          <div>
-            <input
-              onChange={this.handleChange}
-              name="firstName"
-              placeholder="first name"
-              type="text"
-              value={this.props.firstName}/>
-          </div>
-          <div>
-            <input
-              onChange={this.handleChange}
-              name="lastName"
-              placeholder="last name"
-              type="text"
-              value={this.props.lastName}/>
-          </div>
-          <div>
-            <input
-              onChange={this.handleChange}
-              name="instagram"
-              placeholder="@instagram"
-              type="text"
-              value={this.props.instagram}/>
-          </div>
-          <div>
-            <input
-              onChange={this.handleChange}
-              name="camera"
-              placeholder="Go to Camera"
-              type="text"
-              value={this.props.camera}/>
-          </div>
-          <div>
-            <input
-              onChange={this.handleChange}
-              name="lens"
-              placeholder="go to lens"
-              type="text"
-              value={this.props.lens}/>
-          </div>
-          <div>
-            <input
-              onChange={this.handleChange}
-              name="photos"
-              placeholder="a picture of your most recent work"
-              type="text"
-              value={this.props.img1}/>
-          </div>
-          <button type="submit">
-            Submit
-          </button>
-        </form>
-
-        <button onClick="#">Delete</button>
-        </div>
-        </div>
+      </div>
     )
-
-}
-
-
+  }
 }
 
 export default UserEditDelete
