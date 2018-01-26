ddf883c3a90d8880c24284ce62de2f7452d64819
DenimCity <altidorj1@gmail.com>
2018-01-25 23:45:41 -0500

    changes to browser router dom

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {BrowserRouter as Link} from 'react-router-dom'
+import {BrowserRouter as Route, Link} from 'react-router-dom'
 import styled from 'styled-components'
 
 class HomePage extends Component {
