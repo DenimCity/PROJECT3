572224458eedab2966b6a5085125630a21d76f6e
DenimCity <altidorj1@gmail.com>
2018-01-25 22:24:31 -0500

    changed the name of the file

---
+++ client/src/components/EditForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+class EditForm extends Component {
+render(){
+
+
+
+
+  render(
+    <div>
+
+      
+    </div>
+
+  )
+
+}}
+
+export default  EditForm
