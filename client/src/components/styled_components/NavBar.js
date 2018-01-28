953d38029991509d74294e12de75faa385e330f0
DenimCity <altidorj1@gmail.com>
2018-01-28 00:32:46 -0500

    added media-queries packages

---
+++ client/src/components/styled_components/NavBar.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+const NavBar = styled.div `
+background-image: url(https://i.imgur.com/gp9Klik.jpg);
+height: 239px;
+background-position: left;
+width: 100vw;
+position: fixed;
+top: 0;
+overflow: hidden;
+border: transparent;
+a {
+  background-color: transparent;
+    border: 2px solid #fff;
+    color: #fff;
+    border-radius: 300px;
+    letter-spacing: 2px;
+    font-weight: 600;
+    margin-left: 1em;
+    padding: 1em 1.5em !important;
+    text-transform: uppercase;
+    text-decoration:none;
+    line-height:1em;
+  &:hover{
+    background-color:white;
+    color:black;
+  }
+}
+
+`
+
+export default NavBar
