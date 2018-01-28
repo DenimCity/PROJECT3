c36bf638b9fb0408d2c82f0849e3d0806f6b2b34
DenimCity <altidorj1@gmail.com>
2018-01-28 00:34:38 -0500

    created a styled div for the nav bar

---
+++ client/src/components/styled_components/NavLinkWrapper.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const NavLinkWrapper = styled.div`
+display: flex;
+justify-content: flex-end;
+color: #f2f2f2;
+text-align: center;
+padding: 14px;
+text-decoration: none;
+text-align: right;
+font-size: 14px;
+flex-direction: inherit;
+flex-wrap: wrap;
+`
+
+
+
+
+export default NavLinkWrapper
