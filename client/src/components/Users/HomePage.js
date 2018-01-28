c67a8c4908fc68ccab612bcb0753b8c585a3bead
DenimCity <altidorj1@gmail.com>
2018-01-28 17:03:22 -0500

    refactoring the homepage

--- client/src/components/Users/HomePage.js
+++ client/src/components/Users/HomePage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { Link} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import styled from 'styled-components'
 import NavBar from '../styled_components/NavBar'
 import NavLinkWrapper from '../styled_components/NavLinkWrapper'
@@ -8,79 +8,81 @@ import MediaQuery from 'react-responsive'
 class HomePage extends Component {
   render() {
     return (
-      <div className="body">
-          <NavBar>
+      <Wrapper>
+
+        <NavBar>
           <NavLinkWrapper>
-          <Link to="/">Home</Link>
-          <Link to="/user">Users</Link>
-          <a href="">photographers</a>
-          <Link to="#">Photo Gallery</Link>
-          <a href="">About</a>
-          </NavLinkWrapper>
-          </NavBar>
-
-
-          <div className="main">
-          <h1>Welcome to ClickedIn!</h1>
-          
-          
-          
-          </div>
-          
-          
-          
-          <div>
-          
-          <h2>Welcome Visual Creators</h2>
-          
-          
-          </div>
-        {/* <AboveNavBar className="HomePageBackground">
-          <span className="homePageText">
-            
-          </span>
-        </AboveNavBar>
-        <NavBar className="navbar">
-          <div className="button">
             <Link to="/">Home</Link>
-          </div>
-          <div className="button">
             <Link to="/user">Users</Link>
-          </div>
-          <div className="button">
+            <a href="">photographers</a>
             <Link to="#">Photo Gallery</Link>
-          </div>
+            <a href="">About</a>
+          </NavLinkWrapper>
         </NavBar>
-        <Body>
-          <h2>Welcome Visual Creators</h2>
+        <Main1>
+          <TitleHeader>
+            <Img>
+              <img src="https://i.imgur.com/sDVPVPl.jpg" alt=""/>
+            </Img>
+            <Title>
+              <TitleData>
+              <h1>Welcome to ClickedIn!</h1>
+              <div>
+                <p>A page dedicated to photographers and their "Photogmanship. Come one come
+                  all. Become a user and upload away.</p>
+              </div>
+              </TitleData>
+            </Title>
+            <Img>
+              <img src="https://i.imgur.com/sDVPVPl.jpg" alt=""/>
+            </Img>
+          </TitleHeader>
+        </Main1>
+
+        <Main2>
+
+          {/* <h2>Welcome Visual Creators</h2> */}
+
           <p>
-              A page dedicated to photographers and their "Photogmanship. Come one come all.
-              Become a user and upload away.
-            </p>
-            <div className="imageContainer">
-           {/* loop in an array of images in this column */}
-            {/* </div>
-          <div className="textContainer">
-    <h5>Taking Over The World One Click At a Time</h5>
-          </div>
-          <div className="textContainer">
-            <p>
-              With over 10 years of experience in the photography trade, we provided a avenue
-              for our artists. In need of a influencer, Look no further!
-            </p>
-            <div className="image2">
-            </div>
-          </div>
-        </Body> */} 
-      </div>
+            With over 10 years of experience in the photography trade, we provided a avenue
+            for our artists. In need of a influencer, Look no further!
+          </p>
+          </Main2>
+
+      </Wrapper>
     )
   }
 }
 export default HomePage
 
+const Img = styled.div `
+height: 461px;
+width: 70%;
+padding-top: 12px;
+img{
+  width:100%;
+  height:100%;
+}
+`
+const Main1 = styled.div`
+display:flex;
+flex-direction: column;
+`
+const Main2 = styled.div`
+with:100%;
+
+height: 100%;
+display:flex;
+`
+
+const TitleData = styled.div`
+display:flex;
+flex-direction: column;
+align-items:center;
 
 
-const Links = styled.a`
+`
+const Links = styled.a `
 background: transparent;
 border-radius: 300px;
 border: 2px solid #fff;
@@ -95,70 +97,22 @@ margin-left: 1em;
 padding: 1em 1.5em !important;
 display: block;
 `
-// export const AboveNavBar = styled.div`
-// background-image: url(https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200);
-// height: 235px;
-// display: flex;
-// background-position: center;
-// width: 100%;
-// justify-content: center;
-// border: 1px solid;
-// span {
-//   display: flex;
-//   justify-content:row-reverse;
-// }
-// h1{
-//   font-family: cursive;
-//   color: white;
-//   text-shadow: 2px 2px 4px #D2CCA1;
-// }
-// .homePageText {
-//   flex-direction: column-reverse;
-// }`
-// export const NavBar = styled.div`
-// background-color:black;
-//   height: 50px;
-//   display: flex;
-//   flex-direction:row;
-//   justify-content:center;
-//   align-items:center;
-  
-// .button {
-//   margin:2px 50px 2px 50px;
-// }
-//   a {
-//     color:white;
-//   }
-// a:hover {
-//   cursor:pointer;
-//   color:goldenrod;
-// }`
-// const Body = styled.div`
-// h2 {
-//   padding-top:2%;
-//   color: #E83151;
-//     text-align: center;
-//     font-family: serif; 
-// }
-// h5{
-//     color: #E83151;
-//     padding-top:2%;
-//     text-align: center;
-//     font-family: serif;
-// }
-// p {
-//   text-align:center;
-//   font-size:rem;
-//   margin-left: 40px;
-//   margin-right: 40px;
-// }
-// .imageContainer{
-//   background-image:url('https://i.imgur.com/NRnhMjw.jpg')
-//   height:31px;
-//   display:flex;
-//   flex-direction:row;
-// }
-// img {
-//   height:50px;
-//   width:50px;
-// }`
+
+const TitleHeader = styled.div `
+ display:flex;
+ flex-direction:row;
+ `
+
+const Title = styled.div `
+ width: 20%
+ display:flex;
+ flex-direction:column;
+ text-align:center;
+ `
+const Wrapper = styled.div `
+flex-direction: column;
+display: flex;
+justify-content: center;
+width: 100vw;
+height: 100vh;
+`
