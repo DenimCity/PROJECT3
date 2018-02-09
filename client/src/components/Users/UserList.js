2d2a71330d56fc8348a12c6ebc651e4bc5c34824
DenimCity <altidorj1@gmail.com>
2018-02-08 22:56:57 -0500

    page displays users and is mobile friendly

--- client/src/components/Users/UserList.js
+++ client/src/components/Users/UserList.js
@@ -1,64 +1,93 @@
-import React, {Component} from 'react'
-import styled from 'styled-components'
-import {Link} from 'react-router-dom'
-import {connect} from 'react-redux'
-import {push} from 'react-router-redux'
-import {getUserRoute} from '../../actions/thunk.actions'
-import User from './User'
-import NavBar from '../styled_components/NavBar'
-import NavLinkWrapper from '../styled_components/NavLinkWrapper'
+import React, { Component } from "react";
+import styled from "styled-components";
+import { connect } from "react-redux";
+import { push } from "react-router-redux";
+import { getUserRoute } from "../../actions/thunk.actions";
+import User from "./User";
+
 class UserList extends Component {
-
-  // const banana = this.props.map((user, index) => {
-  //   return (<User
-  //     key={index}
-  //     photo={user.photo}
-  //     firstName={user.firstName}
-  //     lastName={user.lastName}
-  //     instagram={user.instagram}
-  //     camera={user.camera}
-  //     lens={user.lens}
-  //     id={user._id}/>)
-  //   })
-  componentWillMount(){
-    this.props.getUserRoute()
+  componentWillMount() {
+    this.props.getUserRoute();
   }
-  
-    
-    render() {
+  render() {
     return (
-      <div>
-        <NavBar>
-          <NavLinkWrapper>
-            <Link to="/">Home</Link>
-            <Link to="/new">Create A User</Link>
-            <Link to="/photographers">Photographers</Link>
-            <Link to="/photogallery">Photo Gallery</Link>
-            {/* <Link to="">About</Link> */}
-          </NavLinkWrapper>
-        </NavBar>
-            <ListWrapper>
-              <Users>
-                {/* {banana} */}
-              </Users>
-            </ListWrapper>
-      </div>
-    )
+      <Wrapper>
+        <CardWrapperContainer>
+          {this.props.users.map((user, i) => {
+            return (
+              <CardWrapper key={i} id={user._id}>
+                <Card>
+                  <PictureContainer>
+                    <img src={user.photo} alt={user.firstName} />
+                  </PictureContainer>
+                  <h1>
+                    {user.firstName} {user.lastName}{" "}
+                  </h1>
+                  <p>Instagram: {user.instagram}</p>
+                  <p>Camera Preference: {user.camera}</p>
+                  <p>Lens Of Choice: {user.lens}</p>
+                  <p>
+                    <button>More Info</button>
+                  </p>
+                </Card>
+              </CardWrapper>
+            );
+          })}
+        </CardWrapperContainer>
+      </Wrapper>
+    );
   }
 }
 
-const mapStateToProps =(state) =>{
-  return {users: state.users}
-}
-export default connect(mapStateToProps, {push, getUserRoute})(UserList)
+const mapStateToProps = state => {
+  return { users: state.users };
+};
+export default connect(mapStateToProps, { push, getUserRoute })(UserList);
 
-const ListWrapper = styled.div `
-display:flex;
-justify-content:center;
-align-items:center;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
 `
-const Users = styled.div `
-display:grid;
-grid-template-columns:33% 33% 33%;
-
+const CardWrapperContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: safe;
+  padding-top: 7rem;
+  width: 75vw;
+`
+const Card = styled.div`
+box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+width: 22rem;
+height:97%;  
+    text-align: center;
+    margin:3px;
+  img{
+    width:100%;
+    height:28rem;
+  }
+  button{
+    border: none;
+    outline: 0;
+    display: inline-block;
+    padding: 8px;
+    color: white;
+    background-color: #000;
+    text-align: center;
+    cursor: pointer;
+    width: 100%;
+    font-size: 18px;
+    
+    &&:hover{
+      opacity: 0.7;
+    }
+`
+const CardWrapper = styled.div`
+  color: red;
+  display: flex;
+  flex-direction: column;
+`
+const PictureContainer = styled.div`
+  width: 22rem;
 `
