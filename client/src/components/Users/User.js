0747ca06c0ef26e7511ca267d453ea03a968f02b
DenimCity <altidorj1@gmail.com>
2018-02-08 22:56:33 -0500

    defactoring component

--- client/src/components/Users/User.js
+++ client/src/components/Users/User.js
@@ -1,64 +1,57 @@
-import React, {  Component} from 'react'
-import {Link} from 'react-router-dom'
-import styled from 'styled-components'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 class User extends Component {
-render() {
-  return(
-    <div>
-
-<UserProfileCardContainer>
-<ProfileContainer>
-<ImageContainer>
-<Link to={`/users/${this.props.id}`}>
-<img src={this.props.photo} alt={this.props.firstName}/>
-</Link>
-</ImageContainer>
-<UserDescriptionContainer>
-<p>First Name: {this.props.firstName} </p>
-<p>Last Name:  {this.props.lastName}</p>
-<p>Instagram: {this.props.instagram} </p>
-<p>Go To Camera: {this.props.camera} </p>
-<p>Got to Lens: {this.props.lens} </p>
-
-</UserDescriptionContainer>
-</ProfileContainer>
-      
-      </UserProfileCardContainer>
-</div>
-  )}}
-
-export default User
-
-
+  render() {
+    return (
+      <div>
+        <UserProfileCardContainer>
+          <ProfileContainer>
+            <ImageContainer>
+              <Link to={`/users/${this.props.id}`}>
+                <img src={this.props.photo} alt={this.props.firstName} />
+              </Link>
+            </ImageContainer>
+            <UserDescriptionContainer>
+              <p>First Name: {this.props.firstName} </p>
+              <p>Last Name: {this.props.lastName}</p>
+              <p>Instagram: {this.props.instagram} </p>
+              <p>Go To Camera: {this.props.camera} </p>
+              <p>Got to Lens: {this.props.lens} </p>
+            </UserDescriptionContainer>
+          </ProfileContainer>
+        </UserProfileCardContainer>
+      </div>
+    );
+  }
+}
+export default User;
 
 const UserProfileCardContainer = styled.div`
-display:flex;
+  display: flex;
 `
 const ProfileContainer = styled.div`
-/* display:flex; */
-/* flex-direction:column; */
+  /* display:flex; */
+  /* flex-direction:column; */
 `
 const ImageContainer = styled.div`
-img {
-  width:200px;
-  @media screen and (max-width:700px){
-width:100px
-  
-}
-}
+  img {
+    width: 200px;
+    @media screen and (max-width: 700px) {
+      width: 100px;
+    }
+  }
 
-@media screen and (max-width:700px){
-
-
-}
+  @media screen and (max-width: 700px) {
+  }
 `
 
 const UserDescriptionContainer = styled.div`
 /* font-size:12px; */
 display:flex;
 flex-direction:column;
-align-content:center
+align-content:center;
 flex-wrap:wrap;
 p{
   font-size:12px;
