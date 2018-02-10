88528f1031bbd1615f4fd7215647e739ca4f91d4
DenimCity <altidorj1@gmail.com>
2018-02-10 13:14:02 -0500

    no longer transferign state from this file. Using Redux to conrol state

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,6 @@
 import React, {Component} from 'react'
 class App extends Component {
   render() {
-    ////the function to grab all the users
-//     const DataOfUsers = () => (<UserList MyUsers={this.state.users} />)
-// ///taking the state from this page and shipping props to their designated folder
-//     const makeNewUser = () => (<NewForm createUser={this.createUser} users={this.state.users}/>)
-//     const editUser = (props) => (<UserEditDelete  users={this.state.users} {...props} />)
-//     const DataOfPhotographers = () => (<PhotographersList MyPhotographers={this.state.photographers} />)
-//     const allOfThePhotos = () => (<PhotoGallery MyPictures={this.state.photos} />)
     return (
       <div>
       </div>
