38e515047f2d0e01af5f259e88ede68c8282b88d
DenimCity <altidorj1@gmail.com>
2018-03-11 15:27:48 -0400

    working on refactored to allow editting of user

--- client/src/components/Users/User.js
+++ client/src/components/Users/User.js
@@ -4,33 +4,30 @@ import { editUserInDatabase, getUser} from "../../actions/thunk.actions";
 import { connect } from "react-redux";
 
 class User extends Component {
-
+  state = {
+    userBeingEdited: {
+      
+    }
+    
+  };
   
   handleChange = (event) => {
     const updateUser = {
-      ...this.props.value
-    };
-    updateUser[event.target.name] = event.target.value;
-    this.setState({ });
+      ...this.state.userBeingEdited
+    }
+    const inputField = event.target.name
+    const inputValue = event.target.value
+
+    updateUser[inputField] = inputValue;
+    this.setState({userBeingEdited:updateUser});
   };
   
   handleSubmit = (event) => {
-    event.preventDefault();  
+    event.preventDefault();
+    this.props.editUserInDatabase(this.state.userBeingEdited)  
 };
-
-componentWillMount(){
-    this.props.getUser(this.props.match.params.userId)
-    // console.log('i have the userID on the client side ', this.props.match.params.userId)
-  }
-  
   
   render() {
-
-    //we ned the 
-    const i = this.props.users
-    // I have the user that match the params of the user match params
-    // console.log('props from the user page is now in the render function', this.props.match.params.userId)
-
     return (
       <div>
 
@@ -44,7 +41,7 @@ componentWillMount(){
               name="photo"
               placeholder="DefaultPhoto"
               type="text"
-              value={this.props.match.params.photo}
+              value={this.state.photo}
             />
           </div>
           <div>
@@ -53,7 +50,7 @@ componentWillMount(){
               name="firstName"
               placeholder="first name"
               type="text"
-              value={this.props.firstName}
+              value={this.state.firstName}
             />
           </div>
           <div>
@@ -102,7 +99,7 @@ componentWillMount(){
 const mapStateToProps = (state) => {
   return { user: state.user }
 }
-export default connect(mapStateToProps, {getUser,editUserInDatabase,})(User);
+export default connect(mapStateToProps, {editUserInDatabase,})(User);
 
 const Submit = styled.button`
   background-color: green;
@@ -111,39 +108,5 @@ const Submit = styled.button`
   &&:hover {
     background-color: goldenrod;
   }
-`;
+`
 
-// const UserProfileCardContainer = styled.div`
-//   display: flex;
-// `
-// const ProfileContainer = styled.div`
-//   /* display:flex; */
-//   /* flex-direction:column; */
-// `
-// const ImageContainer = styled.div`
-//   img {
-//     width: 200px;
-//     @media screen and (max-width: 700px) {
-//       width: 100px;
-//     }
-//   }
-
-//   @media screen and (max-width: 700px) {
-//   }
-// `
-
-// const UserDescriptionContainer = styled.div`
-// /* font-size:12px; */
-// display:flex;
-// flex-direction:column;
-// align-content:center;
-// flex-wrap:wrap;
-// p{
-//   font-size:12px;
-//   @media screen and (max-width:700px){
-// font-size: 8px;
-
-// } 
-// }
-
-// `
