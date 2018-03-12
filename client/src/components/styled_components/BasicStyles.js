79a08defb4242f40631d4720f55fb14cff33b8ce
DenimCity <altidorj1@gmail.com>
2018-03-12 11:30:41 -0400

    changed name of file across components

---
+++ client/src/components/styled_components/BasicStyles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export const TitleContainer = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+font-size:2vw;
+text-align: center;
+font-family: serif;
+
+`
+
+export const Button = styled.button`
+background: #3498db;
+  background-image: -webkit-linear-gradient(top, #3498db, #2980b9);
+  background-image: -moz-linear-gradient(top, #3498db, #2980b9);
+  background-image: -ms-linear-gradient(top, #3498db, #2980b9);
+  background-image: -o-linear-gradient(top, #3498db, #2980b9);
+  background-image: linear-gradient(to bottom, #3498db, #2980b9);
+  -webkit-border-radius: 16;
+  -moz-border-radius: 16;
+  border-radius: 16px;
+  -webkit-box-shadow: 4px 6px 7px #666666;
+  -moz-box-shadow: 4px 6px 7px #666666;
+  box-shadow: 4px 6px 7px #666666;
+  font-family: Arial;
+  color: #ffffff;
+  font-size: 27px;
+  padding: 9px 20px 10px 20px;
+  border: solid #1f628d 2px;
+  text-decoration: none;
+  &:hover {
+    background: #00f04c;
+  text-decoration: none;
+  }
+
+`
