21f929746aa1fcc4c1cbe65eeb6c54737846b676
DenimCity <altidorj1@gmail.com>
2018-03-12 11:30:28 -0400

    removed unused code

--- client/src/components/Users/UserList.js
+++ client/src/components/Users/UserList.js
@@ -2,7 +2,8 @@ import React, {Component} from "react";
 import styled from "styled-components";
 import {connect} from "react-redux";
 import {push} from "react-router-redux";
-import {getUsers, getUser, deleteUserFromDatabase} from "../../actions/thunk.actions";
+import {Button} from '../styled_components/BasicStyles'
+import {getUsers, deleteUserFromDatabase} from "../../actions/thunk.actions";
 import {Link} from 'react-router-dom'
 import User from './User'
 
@@ -56,7 +57,7 @@ class UserList extends Component {
 const mapStateToProps = state => {
   return {users: state.users};
 };
-export default connect(mapStateToProps, {push, getUsers, getUser, deleteUserFromDatabase})(UserList);
+export default connect(mapStateToProps, {push, getUsers, deleteUserFromDatabase})(UserList);
 
 const Wrapper = styled.div `
   display: flex;
@@ -136,28 +137,4 @@ align-items:center;
 margin-top:3rem;
 `
 
-const Button = styled.button`
-background: #3498db;
-  background-image: -webkit-linear-gradient(top, #3498db, #2980b9);
-  background-image: -moz-linear-gradient(top, #3498db, #2980b9);
-  background-image: -ms-linear-gradient(top, #3498db, #2980b9);
-  background-image: -o-linear-gradient(top, #3498db, #2980b9);
-  background-image: linear-gradient(to bottom, #3498db, #2980b9);
-  -webkit-border-radius: 16;
-  -moz-border-radius: 16;
-  border-radius: 16px;
-  -webkit-box-shadow: 4px 6px 7px #666666;
-  -moz-box-shadow: 4px 6px 7px #666666;
-  box-shadow: 4px 6px 7px #666666;
-  font-family: Arial;
-  color: #ffffff;
-  font-size: 27px;
-  padding: 9px 20px 10px 20px;
-  border: solid #1f628d 2px;
-  text-decoration: none;
-  &:hover {
-    background: #00f04c;
-  text-decoration: none;
-  }
 
-`
